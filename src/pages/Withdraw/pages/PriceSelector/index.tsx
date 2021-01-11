617bbdd505c8a7d697ac7ca7d192cf1d08a8c5ca
gabrielhvcardoso <gabrielhvcardoso@gmail.com>
2021-01-11 12:51:26 -0300

    price selector with liomit

--- src/pages/Withdraw/pages/PriceSelector/index.tsx
+++ src/pages/Withdraw/pages/PriceSelector/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import Button from '../../../../components/Button';
 import { Container, PriceInput, Text } from './styles';
 
@@ -7,14 +7,32 @@ import AuthContext from '../../../../context/auth';
 import { ChildProps } from '../../../../components/Switcher';
 import { WithDrawContext } from '../../index';
 import { currencyFormat } from '../../../../utils';
+import { useFetch } from '../../../../hooks';
+import { WithdrawOrder } from '../../../../types/WithdrawOrder';
 
 const PriceSelector: React.FC<ChildProps> = ({ moveToIndex }) => {
   const { current } = useContext(AuthContext);
   const { price, setPrice } = useContext(WithDrawContext);
 
+  const [pendentPayments, setPendentPayments] = useState<number>(0);
+  const [firstType, setFirstType] = useState<boolean>(false);
+
   const error = useMemo<boolean>(() => price === 0, [price]);
 
   if (!moveToIndex || !current) return <></>;
+
+  useEffect(() => {
+    useFetch.get(`/u/w/${current.token}`, (response: Array<WithdrawOrder>) => {
+      const pending = response.filter(item => item.status === 'pending');
+
+      if (pending[0]) {
+        const pendingValue: number = pending.reduce((acc, cur) => acc + cur.amount, 0);
+        setPendentPayments(pendingValue);
+      } else {
+        setPendentPayments(0);
+      }
+    });
+  }, []);
 
   return (
     <Container>
@@ -25,18 +43,21 @@ const PriceSelector: React.FC<ChildProps> = ({ moveToIndex }) => {
         value={currencyFormat(price, true)}
         keyboardType="number-pad"
         onChangeText={(value: string) => {
+          const max = current.account - pendentPayments;
           let unformatted = parseFloat(currencyFormat(value, true).substring(3).replace('.', '').replace(',', '.'));
-          unformatted = unformatted > current.account ? current.account : unformatted;
+          unformatted = unformatted > max ? max : unformatted;
           setPrice(unformatted);
+
+          if (!firstType) setFirstType(true);
         }}
       />
       <Text
-        style={{ color: error ? 'red' : 'black' }}
+        style={{ color: firstType && error ? 'red' : 'black' }}
       >
         {
-          error
+          firstType && error
             ? 'Insira um valor válido'
-            : `Saldo disponível: ${currencyFormat(current.account, true)}`
+            : `Saldo disponível: ${currencyFormat(current.account - pendentPayments, true)}`
         }
       </Text>
 
@@ -45,6 +66,12 @@ const PriceSelector: React.FC<ChildProps> = ({ moveToIndex }) => {
         style={{ marginTop: 30 }}
         onPress={() => moveToIndex(1)}
       >Confirmar</Button>
+
+      {
+        pendentPayments
+          ? <Text style={{ marginTop: 20 }}>{ `${currencyFormat(pendentPayments, true)} em saques pendentes` }</Text>
+          : <></>
+      }
     </Container>
   );
 };
